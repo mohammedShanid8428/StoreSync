297b489a666ecc9ea003fdf454c55e66b165e03e
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-06-12 04:49:27 +0000

    productSlice path corrected p

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productSlice from "./slices/productSlice"
+import productSlice from "./slices/productslice"
 import wishSlice from "./slices/wishSlice"
 import cartSlice from "./slices/cartSlice"
 
